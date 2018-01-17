079c68fa502397c734268a84eb08b8f11ceb8b10
wcss2010 <wcss2020@gmail.com>
2018-01-17 16:17:41 +0800

    First

---
+++ src/face/opencv3/videocapture/demo2/CaptureBasicPanel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package face.opencv3.videocapture.demo2;
+
+/**
+ *
+ * @author wcss2
+ */
+public class CaptureBasicPanel {
+    
+}
